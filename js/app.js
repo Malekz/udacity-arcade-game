cda1123d5937b10ff78d4d26ac4464ab5bae6fb5
malekz <malek.zarzour@gmail.com>
2019-07-21 23:49:02 +0200

    update The enemy Position

--- js/app.js
+++ js/app.js
@@ -11,3 +11,32 @@ var Enemy = function(x, y, speed) {
   // a helper we've provided to easily load images
   this.sprite = "images/enemy-bug.png";
 };
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x = this.x + this.speed * dt;
+
+  // reset bugs position when hit border
+  if (this.x > 600) {
+    this.x = -50;
+  }
+
+  // reset player position when collide with enemy-bug
+  if (
+    player.x > this.x - 50 &&
+    player.x < this.x + 50 &&
+    player.y > this.y - 50 &&
+    player.y < this.y + 50
+  ) {
+    player.resetPlayer();
+  }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
